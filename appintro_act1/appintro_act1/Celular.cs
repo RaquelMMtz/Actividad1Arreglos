0efebd1036ca1bab9fa2162dd31642e8eb2a77cc
RaquelMMtz <70656439+RaquelMMtz@users.noreply.github.com>
2021-09-04 22:50:46 -0500

    Add files via upload

---
+++ appintro_act1/appintro_act1/Celular.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appintro_act1
+{
+    class Celular
+    {
+        string  marca;
+        int año;
+        string color;
+        string telefonia;
+        int memoria;
+        public string  Marca 
+        {
+            get { return marca; }
+            set { marca= value; }
+        }
+        public int Año
+        {
+            get { return año; }
+            set { año = value; }
+        }
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        public string Telefonia
+        {
+            get { return telefonia; }
+            set { telefonia = value; }
+        }
+        public int Memoria
+        {
+            get { return memoria; }
+            set { memoria = value; }
+        }
+
+        public override string ToString()
+        {
+            return marca + año + " " + color + telefonia + memoria + " ";
+        }
+    }
+}
---
+++ appintro_act1/appintro_act1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace appintro_act1
+{
+    public partial class Form1 : Form
+    {
+        string info;
+        int i = 0;
+        Celular[] array = new Celular[10];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            array[i] = new Celular();
+            array[i].Marca = textBox1.Text;
+            array[i].Año = int.Parse(textBox2.Text);
+            array[i].Color = textBox3.Text;
+            array[i].Telefonia = textBox4.Text;
+            array[i].Memoria = int.Parse(textBox5.Text);
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            i++;
+        }
+    }
+}
